0da41e6b329877d790ac837b8b1cea364fd8ef54
DHrushikesh <hrushikesh2003@gmail.com>
2026-05-23 16:06:48 +0530

    added dot env

--- middleawre/middleware.js
+++ middleawre/middleware.js
@@ -1,4 +1,8 @@
 import jwt from 'jsonwebtoken';
+import { omitUndefined } from 'mongoose';
+import dotenv from 'dotenv';
+
+dotenv.config();
 
 const JWT_SECRET = process.env.JWT_SECRET || 'dev_secret';
 
